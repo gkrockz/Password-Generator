a82be1b8ae038507ea25640ac327f1a23b3ef278
Kishore G <kishoregk2001@gmail.com>
2021-01-09 11:01:35 +0530

    Added Strength Feature, To PassGenerator.

--- PassGenerator.py
+++ PassGenerator.py
@@ -11,5 +11,6 @@ upr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 num = "0123456789"
 sp_chars= "&@!$*-_#"
 Pass = lwr + sp_chars + num + upr
-length = 12
+strength =  input("Enter The Strength Of The Password Required - (Weak/Strong/VeryStrong) : ")
+length = 6 if (strength == "Weak") else  12 if (strength =="Strong") else 18
 GeneratePassword(Pass,length)
