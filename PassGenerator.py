5efd7b406c9ef95e27d12c99ec6570ab8132114a
Kishore G <61354468+gkrockz@users.noreply.github.com>
2021-04-05 15:32:00 +0530

    Updated Comment

--- PassGenerator.py
+++ PassGenerator.py
@@ -8,9 +8,12 @@ lwr = "abcdefghijklmnopqrstuvwxyz"
 upr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 num = "0123456789"
 sp_chars= "&@!$*-_#"
-Pass = lwr + sp_chars + num + upr      # Combining All Types Patterns.
+
+# Combining All Characters.
+Pass = lwr + sp_chars + num + upr      
 
 # Using Ternary Operator To Fix The Length Of The Password , Based On User Input.
+
 strength = input(" Enter The Strength Of The Password Required - ( Weak / Strong / Very Strong) : ")
 if strength.isalpha() == True :
     length = 6 if (strength == "weak") else 12 if (strength =="strong") else 18
